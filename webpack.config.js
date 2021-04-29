f074801704bea9b3532e7d766000a2ff58f38b2e
georges <email@example.com>
2021-04-29 19:42:25 +0200

    CV ready for prouction

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,53 @@
 const path = require('path');
-//const loader = require('sass-loader');
 
 
-module.exports = {
-    entry : './src/app.js',
-    output : {
-        path:path.join(__dirname, 'public'),
-        filename : 'bundle.js'
-    },
-    module : {
-        rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            loader : 'babel-loader',
-        }, 
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader?url=true',
-                'sass-loader'
-            ]
+module.exports = (env) => {
+    const isProduction = env == 'production';
+    return {
+        entry : './src/app.js',
+        output : {
+            path:path.join(__dirname, 'public'),
+            filename : 'bundle.js'
         },
-        {
-            test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-            use: [
-                    {
-                    loader: 'url-loader',
-                        
-                    options: {
-                    limit: 8192, // if less than 10 kb, add base64 encoded image to css 
-                    name: '[name].[hash:7].[ext]'
+        module : {
+            rules: [{
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader : 'babel-loader',
+            }, 
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader?url=true',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+                use: [
+                        {
+                        loader: 'url-loader',
+                            
+                        options: {
+                        limit: 8192, // if less than 10 kb, add base64 encoded image to css 
+                        name: '[name].[hash:7].[ext]'
+                    }
+                  },
+                  {
+                        loader: 'img-loader',
                 }
-              },
-              {
-                    loader: 'img-loader',
-            }
+                ]
+            },
             ]
-        },
-        ]
-    },    
-    devtool : 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback:true
+        },    
+        devtool : isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback:true
+        }
+     
     }
+           
 };
 
